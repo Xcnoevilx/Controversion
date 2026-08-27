94fa31bde98ce2746cb6be6aaa87b58522c793e1
Xcnoevilx <145766722+Xcnoevilx@users.noreply.github.com>
2026-08-26 17:26:54 -0700

    Add application configuration classes

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.getenv(
+        "SECRET_KEY",
+        "development-only-secret"
+    )
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///controversion.db"
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig
+}
